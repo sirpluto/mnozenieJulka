0632d3a5706c003fe71a8135ba8fa5216b9d469b
pluto <grzegorz.pluto@gmail.com>
2019-10-02 22:56:56 +0200

    aktualizacja nagrod

--- utils.py
+++ utils.py
@@ -3,23 +3,15 @@ import subprocess
 import sys
 import random
 
-nagrody = {1  : ["Barbie w swiecie mody",                   "https://www.youtube.com/watch?v=P0oaTAuzB70"],
-           2  : ["Barbie i Jej Siostry w Krainie Kucykow",  "https://www.youtube.com/watch?v=oMeH-3IZ04o"],
-           3  : ["Barbie: Tajne agentki",                   "https://www.youtube.com/watch?v=tE5tYShcAs0"],
-           4  : ["Barbie w swiecie gier ",                  "https://www.youtube.com/watch?v=GLbJH6E-8BQ"],
+nagrody = {1  : ["Barbie w swiecie mody",                   "https://www.youtube.com/watch?v=MVt4CiCbU9s"],
+           2  : ["Barbie i Jej Siostry w Krainie Kucykow",  "https://www.youtube.com/watch?v=8KyCaPaOJjc"],
+           3  : ["Barbie: Tajne agentki",                   "https://www.youtube.com/watch?v=Qnyyc6JEgv8"],
+           4  : ["Barbie w swiecie gier ",                  "https://www.youtube.com/watch?v=zxGeFeTerA0"],
            5  : ["Barbie i Tajemnicze Drzwi",               "https://www.youtube.com/watch?v=1dwxBP0zed4"],
            6  : ["Barbie i magiczne baletki",               "https://www.youtube.com/watch?v=G0W3p0mrHK0"],
-           7  : ["Barbie i Akademia Ksiezniczek",           "https://www.youtube.com/watch?v=iiij2ApJg_c"],
+           7  : ["Barbie i Akademia Ksiezniczek",           "https://www.youtube.com/watch?v=T6rx-BeRQEQ"],
            8  : ["WIELKI WYSCIG ",                          "https://www.youtube.com/watch?v=ISnS0z4TkHk&t=443s"],
            9  : ["DZIECIAKI MOJEJ SIOSTRY W AFRYCE",        "https://www.youtube.com/watch?v=g96u-yBL1-s"],
-           10 : ["DZIECI Z BULLERBYN ",                     "https://www.youtube.com/watch?v=yVgSgxtqjNU"],
-           11 : ["Beethoven",                               "https://www.cda.pl/video/51526636"],
-           12 : ["Dzieciak rzadzi",                         "https://www.cda.pl/video/14296411e"],
-           13 : ["Kacper / Casper",                         "https://www.cda.pl/video/1110078ea"],
-           14 : ["Zwierzogrod",                             "http://ekino-tv.pl/movie/show/zwierzogrod-hd-zootopia-2016-dubbing/15191"],
-           15 : ["High School Musical",                     "https://www.cda.pl/video/561057e7"],
-           16 : ["Asterix misja Kleopatra",                 "https://www.cda.pl/video/138914f6"],
-           17 : ["Emoticon",                                "https://www.cda.pl/video/1756135c3"],
            }
 
 webBrowser = {"win"   : "start microsoft-edge:",
@@ -41,7 +33,7 @@ def run_game():
     #for nr in nagrody:
     #    print(str(nr) + " : " + nagrody[nr][0])
     #wybor = int(input("Wybierz nagrode: "))
-    wybor = random.randint(1, 17)
+    wybor = random.randint(1, 9)
 
     proc = subprocess.Popen(str(webBrowser[ver_os()] + nagrody[wybor][1]), shell = True)
     return proc
